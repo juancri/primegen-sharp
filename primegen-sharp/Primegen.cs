ba078f99142af84bbe47aa8114e344fe142e5bc4
Juan C. Olivares <cristobal@cxsoftware.com>
2016-01-22 02:02:20 -0300

    First commit

---
+++ primegen-sharp/Primegen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace primegensharp
+{
+	public static class Primegen
+	{
+		// Constructor
+
+		public static IEnumerable <ulong> Get ()
+		{
+			var pg = PrimegenStruct.Create ();
+			primegen_init(ref pg);
+			while (true)
+				yield return primegen_next (ref pg);
+		}
+
+
+
+		// External
+
+		[DllImport ("primegen")]
+		private static extern void primegen_init(ref PrimegenStruct pg);
+
+		[DllImport ("primegen")]
+		private static extern ulong primegen_next(ref PrimegenStruct pg);
+	}
+}
+
---
+++ primegen-sharp/PrimegenStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace primegensharp
+{
+	[StructLayout (LayoutKind.Sequential)]
+	public struct PrimegenStruct
+	{
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 16 * 2048)]
+		public uint[] buf;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+		public ulong[] p;
+		public int num;
+		public int pos;
+		public ulong baseNumber;
+		public ulong L;
+
+		public static PrimegenStruct Create ()
+		{
+			return new PrimegenStruct
+			{
+				buf = new uint [16 * 2048],
+				p = new ulong [512]
+			};
+		}
+	}
+}
+
